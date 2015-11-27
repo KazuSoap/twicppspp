32615a9f7a642ade8d136b9c387edc9c5ecd8b2d
KazuSoap <Kazu.Soap@gmail.com>
2015-11-27 20:04:54 +0900

    deleted

--- test/include/KazuSoap_key.h
+++
@@ -1,5 +0,0 @@
-// 作成したアプリの署名キー
-static char const c_key[] = "vBcjiCX0ip2Aj9Esi1qmEXqFF";
-static char const c_sec[] = "tYIKxQS78YuwhDZHKbUa45b0I8KZzbezDK7ROPBYO9ijJyBwUA";
-static char const t_key[] = "4268495780-bXES7qL5XYugu3u6xk6iqM1ZyipTAYWVARV7v2u";
-static char const t_sec[] = "ibsw9HmNZAISVAb7MqrSIFP1DiFOUDzWrtq2cVeESQ1Zc";
--- test/include/mykey.h
+++
@@ -1,4 +0,0 @@
-static char const c_key[] = "xxxxxxxxxxxxxxxxxxxx";
-static char const c_sec[] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-static char const t_key[] = "00000000-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-static char const t_sec[] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
